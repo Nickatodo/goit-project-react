961adc327cd565966db5ae71b6a6c2e343051bd1
Nicolas Canales Diaz <nicolasjcanalesdiaz@gmail.com>
2024-07-08 15:45:54 -0400

    Correcion ProtectedRoute

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -4,5 +4,9 @@ import { Navigate } from 'react-router-dom';
 export default function ProtectedRoute({ element: Element, ...rest }) {
   const isLogged = useSelector(state => state.auth.isLogged);
 
-  return isLogged ? <Element {...rest} /> : <Navigate to="/login" />;
+  return isLogged ? (
+    <Element {...rest} />
+  ) : (
+    <Navigate to="/goit-project-react/login" />
+  );
 }
